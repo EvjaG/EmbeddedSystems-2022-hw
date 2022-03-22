59667314c2ebf38002881ef64bed2a069764a836
EvjaG <63923534+EvjaG@users.noreply.github.com>
2022-03-22 14:41:08 +0200

    Addest usart2 to ex1, wrote all main functional funcs
    to-do: write the functions that respond to button, timer and input

--- Class/prints/Debug/Src/subdir.mk
+++ Class/prints/Debug/Src/subdir.mk
@@ -18,8 +18,13 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Src/main.o: ../Src/main.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F3 -DDEBUG -DSTM32F303RETx -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
-Src/usart2.o: ../Src/usart2.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F3 -DDEBUG -DSTM32F303RETx -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/usart2.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Src/%.o: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F3 -DDEBUG -DSTM32F303RETx -c -I../Inc -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/main.d ./Src/main.o ./Src/usart2.d ./Src/usart2.o
+
+.PHONY: clean-Src
 
--- EX1/Src/main.c
+++ EX1/Src/main.c
@@ -3,12 +3,115 @@
 /* All references to the datasheet are to STM's RM0316, revision 8           */
 /***************************/
 #include "stm32f303xe.h"
-
+#include <stdio.h>
+#include <string.h>
+#include "usart2.c"
 /*
  * 8mhz= 8*(10**6)/sec
  * 4*10**6 = time = 3D0900
  *
  * */
+
+int second=0;
+int minute=0;
+int hour=0;
+
+//this will be the button interrupt function
+char* returnHour(){
+    char* toReturn = (char*) malloc(10*sizeof(char));
+	sprintf(toReturn,"%02d:%02d:%02d\n",hour,minute,second);
+	return toReturn;
+}
+
+int power(int a,int b){
+	int toReturn=1;
+	for (int i=0;i<b;i++)
+		toReturn*=a;
+	return toReturn;
+}
+//this will be the timer interrupt function
+void increaseSec(){
+	minutePass=0;
+	HourPass=0;
+	second++;
+	if(second==60){
+		second=0;
+		minutePass=1;
+	}
+	if(minutePass){
+		minute++;
+		if(minute==60){
+				second=0;
+				hourPass=1;
+		}
+	}
+	if(hourPass){
+			hour++;
+			if(hour==24){
+					hour=0;
+					hourPass=1;
+			}
+		}
+
+}
+
+//this will be the function from reading the input from the user
+//return 0 if fail, otherwise return 1
+int inputTime(char* input){
+	//parse string - check for correct format
+	int len;
+	colon_count=0;
+	func_count=0;
+	space_count=0;
+	for (len = 0; input[len] != '\0'; len++){
+		if(input[len]==':') colon_count++;
+		if(input[len]==' ') space_count++;
+		if(input[len]=='-'){
+			if(len>0 || func_count)
+				return 0;
+			func_count++;
+		}
+
+	}
+	if(colon_count != 2 || func_count != 1) return;
+	//if func call not in system
+	if(strstr(input,"-time")==NULL)
+		return 0;
+
+	//check string inputs
+	char *token = strtok(str, " ");
+	token = strtok(NULL, ":");
+
+	int j=0;
+	int modArr[3]={60,60,24};
+	int timeArr[3]={0,0,0};
+
+	while (token != NULL)
+	{
+//		printf("String:\t%s\n", token);
+        int sum=0;
+		for(int i=0;i<2;i++){
+			int c = token[i];
+			if(c<48 || c>57) //check if char is digit
+				return 0;
+			c-=48;
+			sum+=c*power(10, 2-(i+1));
+		}
+		if(sum<0 || sum>=modArr[j])
+			return 0;
+//		printf("sum:\t%d\n",sum);
+		timeArr[j]=sum;
+
+		token = strtok(NULL, ":");
+		j++;
+	}
+	//if no errors, change time vars accordingly
+	hour=timeArr[0];
+	minute=timeArr[1];
+	second=timeArr[2];
+
+}
+
 
 
 
